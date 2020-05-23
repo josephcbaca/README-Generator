9e9a09ce2aca8ba60441533868abf2f71634f107
Joseph Baca <josephcbaca@gmail.com>
2020-05-23 06:03:17 -0700

    Pseudo-code plus initial code

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
+// Link to generateMarkdown.js which will write our information from prompt to .md
+let generate = require("./utils/generateMarkdown.js");
+// 
+let fs = require("fs");
+// 
+let inquirer = require("inquirer");
+
+let fileName = fs.writeFile("README.md");
+
+
+console.log(generate.welcome);
+
 const questions = [
 
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "checkbox",
+            message: "What languages do you know?",
+            name: "stack",
+            choices: [
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "MySQL"
+            ]
+        },
+        {
+            type: "list",
+            message: "What is your preferred method of communication?",
+            name: "contact",
+            choices: [
+                "email",
+                "phone",
+                "telekinesis"
+            ]
+        }
+    ])
 ];
 
+questions.then(function(data) {
+
+    var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+  
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+  
+      if (err) {
+        return console.log(err);
+      }
+  
+      console.log("Success!");
+  
+    });
+
 function writeToFile(fileName, data) {
+
 }
 
 function init() {
@@ -10,3 +66,18 @@ function init() {
 }
 
 init();
+
+// User types node index.js to start command-line questions
+// Welcome message
+
+// Prompts for title
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions -- What does this mean?
+    // User GitHub profile picture
+    // User GitHub email
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,7 @@
+let welcome = "Welcome to README.md generator, your one stop shop to creating a Read Me file.";
+let start = "Would you like to create a README.md?"
+
+
 function generateMarkdown(data) {
   return `
 # ${data.title}
@@ -5,4 +9,10 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  welcome: welcome,
+  generateMarkdown: generateMarkdown
+
+};
+
+
